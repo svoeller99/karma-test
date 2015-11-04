7342df63786605508a3f3b5a58235cc99cac4d82
Sean Voeller <sean.voeller@gmail.com>
2015-11-03 19:57:26 -0800

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var Server = require('karma').Server;
+
+['test','tdd'].forEach(function(taskName){
+  gulp.task(taskName, function(done){
+    new Server({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: taskName === 'test'
+    }, done).start();
+  });
+});
